bf6fd8d60efc1b6ac8e99bf9893407c81fe2b695
rockleeprc <rockleeprc@outlook.com>
2018-07-20 17:24:05 +0800

    初始化项目

---
+++ spring-boot-rigger/rigger-web/src/main/java/com/rigger/SpringBootRiggerApplication.java
@@ -0,0 +1,12 @@
+package com.rigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRiggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRiggerApplication.class, args);
+	}
+}
