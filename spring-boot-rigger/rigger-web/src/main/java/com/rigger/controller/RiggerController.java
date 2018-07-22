d7bf0ccfdae90c41b63170b03d3e3e472ef48f1a
Rock Lee <rockleeprc@outlook.com>
2018-07-22 16:39:48 +0800

    增加req接口用于测试

--- spring-boot-rigger/rigger-web/src/main/java/com/rigger/controller/RiggerController.java
+++ spring-boot-rigger/rigger-web/src/main/java/com/rigger/controller/RiggerController.java
@@ -2,9 +2,12 @@ package com.rigger.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController()
 @RequestMapping("/rigger")
@@ -17,6 +20,7 @@ public class RiggerController {
         log.info("log....");
         return "Hello spring-boot-rigger";
     }
+
     @GetMapping("/fail")
     public String fail() {
         log.info("fail....");
@@ -26,6 +30,13 @@ public class RiggerController {
         }
         int i=1/0;
         return "Hello spring-boot-rigger";
+    }
+
+
+    @GetMapping("req")
+    public void req(HttpServletRequest request, @CookieValue(value="JESSIONID",required = false) String sessionId){
+        System.out.println("sessionId="+request.getSession().getId());
+        System.out.println("sessionId="+sessionId);
     }
 
 
