6e983803936bc16dad1024ff412cf748bacd59b9
rockleeprc <rockleeprc@outlook.com>
2018-07-24 18:05:23 +0800

    jwt工具类

---
+++ spring-boot-rigger/rigger-web/src/test/java/jwt/JwtTest.java
@@ -0,0 +1,29 @@
+package jwt;
+
+import com.auth0.jwt.interfaces.Claim;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class JwtTest {
+
+    private static final Long userId=12345678987654321L;
+    @Test
+    public void testCreate() throws InterruptedException {
+        String token = JwtToken.createToken(userId);
+        System.out.println(token);
+
+        TimeUnit.SECONDS.sleep(2);
+
+        System.out.println(JwtToken.isExpire(token));
+        if(JwtToken.isExpire(token)){
+            System.out.println(JwtToken.isExpire(token));
+        }else {
+            Map<String, Claim> result = JwtToken.verifyToken(token);
+            System.out.println(result.get("userId").asString());
+            result.keySet().forEach(key -> System.out.println(key));
+        }
+    }
+
+}
