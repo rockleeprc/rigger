10aeef46fd0462fef344f93a08b9ad650447011e
rockleeprc <rockleeprc@outlook.com>
2018-07-27 11:28:43 +0800

    增加Result测试类

--- spring-boot-rigger/rigger-web/src/main/java/com/rigger/common/Result.java
+++ spring-boot-rigger/rigger-web/src/main/java/com/rigger/common/Result.java
@@ -94,30 +94,4 @@ public class Result<T> implements Serializable {
                 ", data=" + data +
                 '}';
     }
-
-    public static void main(String[] args) {
-//        System.out.println(JsonMapper.objectToJson(Result.ok()));
-//        System.out.println(JsonMapper.objectToJson(Result.ok(Arrays.asList("1","AA","VV"))));
-//        System.out.println(JsonMapper.objectToJson(Result.no(SystemCode.UNAUTHORIZED,"认证失败")));
-//        System.out.println(JsonMapper.objectToJson(Result.no(SystemCode.DATA_ALREADY_EXISTED)));
-//        System.out.println(JsonMapper.objectToJson(Result.no(SystemCode.DATA_ALREADY_EXISTED,Arrays.asList("1","2","4"))));
-
-//        String j1 = JsonMapper.objectToJson(Result.ok());
-//        Result r1 = JsonMapper.jsonToObject(j1, Result.class);
-//        System.out.println(r1);
-//
-        String j2 = JsonMapper.objectToJson(Result.ok(Arrays.asList("1", "AA", "VV")));
-        Result r2 = JsonMapper.jsonToObject(j2, Result.class);
-        System.out.println(r2);
-
-        String j1 = JsonMapper.objectToJson(Result.ok());
-        Result r1 = FastJsonUtil.parseToClass(j2, Result.class);
-        System.out.println(r1);
-
-        String j3 = JsonMapper.objectToJson(Result.ok(Arrays.asList("1", "AA", "VV")));
-        Result r3 = FastJsonUtil.getObject(j2, Result.class);
-        System.out.println(r2);
-
-
-    }
 }
---
+++ spring-boot-rigger/rigger-web/src/test/java/com/rigger/common/ResultTest.java
@@ -0,0 +1,39 @@
+package com.rigger.common;
+
+import com.rigger.util.FastJsonUtil;
+import com.rigger.util.JsonMapper;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class ResultTest {
+
+    @Test
+    public void testParseResult(){
+        String j1 = JsonMapper.objectToJson(Result.ok());
+        Result r1 = JsonMapper.jsonToObject(j1, Result.class);
+        System.out.println(r1);
+
+        String j2 = JsonMapper.objectToJson(Result.ok(Arrays.asList("1", "AA", "VV")));
+        Result r2 = JsonMapper.jsonToObject(j2, Result.class);
+        System.out.println(r2);
+
+        String j3 = JsonMapper.objectToJson(Result.ok(Arrays.asList("1", "AA", "VV")));
+        Result r3 = FastJsonUtil.getObject(j2, Result.class);
+        System.out.println(r3);
+
+        String j4 = JsonMapper.objectToJson(Result.ok());
+        Result r4 = FastJsonUtil.parseToClass(j4, Result.class);
+        System.out.println(r4);
+
+    }
+
+    @Test
+    public void testCreateResult(){
+        System.out.println(JsonMapper.objectToJson(Result.ok()));
+        System.out.println(JsonMapper.objectToJson(Result.ok(Arrays.asList("1","AA","VV"))));
+        System.out.println(JsonMapper.objectToJson(Result.no(SystemCode.UNAUTHORIZED,"认证失败")));
+        System.out.println(JsonMapper.objectToJson(Result.no(SystemCode.DATA_ALREADY_EXISTED)));
+        System.out.println(JsonMapper.objectToJson(Result.no(SystemCode.DATA_ALREADY_EXISTED,Arrays.asList("1","2","4"))));
+    }
+}
